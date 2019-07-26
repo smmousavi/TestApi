9968739c6448c0a5c42bdc62304ce86c2187f30c
mousavi <smmousavi_ir@yahoo.com>
2019-07-26 17:55:44 +0430

    student model added

---
+++ TestAPI/Models/Model.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestAPI.Models
+{
+    public class Model
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ TestAPI/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestAPI.Models
+{
+    public class Student
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+    }
+}
